12767dd2543034de35ea0e56d60276539a8fd466
Satyendra Kumar <satyendra.k13@iiits.in>
2017-02-10 00:29:01 +0530

    Assignment-2

---
+++ Assignments/Assignment-2/Function_Call.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require 'ffi'
+ 
+module LibC
+  extend FFI::Library
+  ffi_lib FFI::Platform::LIBC
+ 
+  attach_function :strdup, [:string], :pointer
+  attach_function :free, [:pointer], :void
+end
+ 
+string = "Hello, World!"
+duplicate = LibC.strdup(string)
+puts duplicate.get_string(0)
+LibC.free(duplicate)
